c8d5f123e985d1dc083d4aa911a9d0060688e007
g905 <breakestra@yandex.ru>
2019-10-30 18:16:27 +0400

    scroll top, list view, sidebar etc

--- src/js/index.js
+++ src/js/index.js
@@ -11,8 +11,8 @@ import 'slick-carousel'
 //import List from 'list.js'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faMapMarkerAlt, faSearch, faChevronDown, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
-library.add( faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram, faMapMarkerAlt, faSearch, faChevronDown, faChevronLeft, faChevronRight );
+import { faMapMarkerAlt, faSearch, faChevronDown, faChevronLeft, faChevronRight, faTh, faThList } from '@fortawesome/free-solid-svg-icons'
+library.add( faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram, faMapMarkerAlt, faSearch, faChevronDown, faChevronLeft, faChevronRight, faTh, faThList );
 
 // ====================================== Replace fa-icons with SVGs ===========================
 dom.watch();
@@ -34,13 +34,92 @@ $(()=>{
         $('.animated-icon1').toggleClass('open');
     });
 
-// ====================================== YMaps ================================================
+// ===================================== Custom File Input =====================================
+
+    $('#customFile').change((e)=>{
+        let filename = $(e.target).val();
+        filename = filename.substring(filename.lastIndexOf("\\") + 1, filename.length);
+        $(e.target).next('.custom-file-label').html(filename);
+    });
+
+// =================================== scroll to top button ====================================
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.scrollTop').fadeIn();
+        } else {
+            $('.scrollTop').fadeOut();
+        }
+    });
+    $('.scrollTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+// ================================== View of Items in Catalog =================================
+
+    function displayItems(){
+        let sideBarActive = window.localStorage.getItem('sidebar');
+        let viewItems = window.localStorage.getItem('view');
+        if(sideBarActive === 'true') {
+            $('.custom-switch input').attr('checked', true);
+            $('.left-bar').removeClass('collapsed');
+        } else {
+            $('.custom-switch input').attr('checked', false);
+            $('.left-bar').addClass('collapsed');
+            $('.left-bar .collapsible').removeClass('show');
+            $('.accordion-card').removeClass('active');
+        }
+
+        $('.view').removeClass('active');
+
+        if(viewItems === 'list'){
+            $('.catalog-items').addClass('list-view');
+            $('[data-view = list]').addClass('active');
+        } else {
+            $('.catalog-items').removeClass('list-view');
+            $('[data-view = tile]').addClass('active');
+        }
+
+        if(window.innerWidth <= 768) {
+            $('.custom-tile').css('display', 'none');
+            $('.catalog-items').removeClass('list-view');
+        }
+    }
+
+    displayItems();
+
+    $('.view').click((e) => {
+        if($(e.target).closest('div').hasClass('active')) {
+            return false;
+        }
+        let viewItem = $(e.target).closest('div');
+        let viewType = viewItem.data('view');
+        $('.view').removeClass('active');
+        viewItem.addClass('active');
+        if(viewType === 'list'){
+            $('.catalog-items').animate({'opacity': 0}, 200, function(){
+                $('.catalog-items').addClass('list-view');
+            });
+            $('.catalog-items').animate({'opacity': 1}, 200);
+            window.localStorage.setItem('view', 'list');
+        } else {
+            $('.catalog-items').animate({'opacity': 0}, 200, function(){
+                $('.catalog-items').removeClass('list-view');
+            });
+            $('.catalog-items').animate({'opacity': 1}, 200);
+            window.localStorage.setItem('view', 'tile');
+        }
+    });
+
+// ========================================== YMaps ============================================
 
     ymaps.ready(init);
     function init(){
         let myMap = new ymaps.Map("ymapsContainer", {
             center: [55.76, 37.64],
-            zoom: 6,
+            zoom: 1,
             controls: []
         });
 
@@ -75,6 +154,33 @@ $(()=>{
                 });
 
             }
+        }
+
+        if($('#modalMap').length){
+            console.log('modal map container exists');
+            let modalMap = new ymaps.Map("modalMap", {
+                center: [55.76, 37.64],
+                zoom: 11,
+                controls: []
+            });
+            var addr = ['хамовники', 'арбат', 'электросталь'];
+            console.log(addr);
+
+            ymaps.geocode(addr, {
+                results: 1
+            }).then(function(res){
+                let firstObj = res.geoObjects.get(0),
+                    coordinates = firstObj.geometry.getCoordinates(),
+                    bounds = firstObj.properties.get('boundedBy');
+                firstObj.options.set('preset', 'islands#darkBlueDotIconWithCaption');
+                firstObj.properties.set('iconCaption', firstObj.getAddressLine());
+                modalMap.geoObjects.add(firstObj);
+                // Масштабируем карту на область видимости геообъекта.
+                modalMap.setBounds(bounds, {
+                    // Проверяем наличие тайлов на данном масштабе.
+                    checkZoomRange: true
+                });
+            });
         }
 
         let route = new ymaps.multiRouter.MultiRoute({
@@ -208,7 +314,7 @@ $(()=>{
 
         $('#calculator').removeClass('calc-show');
         $('.screen').fadeOut();
-        $('#calculator, .filters').find('.active').removeClass('active');
+        $('#calculator, .filters, #faq #question').find('.active').removeClass('active');
     };
 
     $('.rightBtn').click((e)=>{
@@ -232,14 +338,23 @@ $(()=>{
 
 // ========================================= Switcher ==========================================
 
-    $('.custom-switch input').change((e) => {
-        console.log(e.target.checked)
+    $('.custom-switch input').click((e) => {
+        console.log(e.target.checked);
         if(e.target.checked) {
             $('.left-bar').removeClass('collapsed');
         } else {
             $('.left-bar').addClass('collapsed');
+            $('.left-bar .collapsible').removeClass('show');
+            $('.accordion-card').removeClass('active');
         }
+        window.localStorage.setItem('sidebar', e.target.checked);
     });
+
+// ======================================== Accordion ==========================================
+    //$('.accordion-card').click(function(e){
+        //$(this).find('.collapsible').collapse('toggle');
+        //$(this).toggleClass('active');
+    //});
 
 // ====================================== Slick sliders ========================================
 
